d337e3d33704e51dbbee33847a6279bb39c42922
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-15 19:59:57 +0530

    How many weeks of your life is remaining?

---
+++ Your_Life_In_Weeks.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age=int(age)
+max_age=90
+remaining_age=max_age-age
+remaining_days=remaining_age*365
+remaining_weeks=remaining_age*52
+remaining_months=remaining_age*12
+
+print(f"You have {remaining_days} days, {remaining_weeks} weeks, and {remaining_months} months left")
+
+
+
+
+
+
+
+
+
