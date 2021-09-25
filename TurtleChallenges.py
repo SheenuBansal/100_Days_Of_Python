733407c0e325cf65c9b0d1e1f88b6568c19827da
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-25 20:16:02 +0530

    Draw a Dashed Line or Square

---
+++ TurtleChallenges.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+my_screen=Screen()
+kimmy=Turtle()
+
+# # --------------------- CHALLENGE 1 -----------------------------
+# # ------------------Draw a SQAURE--------------------------------
+# for _ in range(4):
+#     kimmy.forward(100)
+#     kimmy.left(90)
+#
+# my_screen.exitonclick() 
+
+# # --------------------- CHALLENGE 2 -----------------------------
+# --------------------DRAW a DASHED Line --------------------------
+# for i in range (10):
+#     kimmy.pendown()
+#     kimmy.forward(15)
+#     kimmy.penup()
+#     kimmy.forward(15)
+#
+# my_screen.exitonclick() 
+
