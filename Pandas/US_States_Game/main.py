e8210a41415f9f48c0acc3572dc84ae21d376c17
Sheenu Bansal <sheenubansal308@gmail.com>
2021-10-05 18:03:50 +0530

    Modified using List Comprehension

--- Pandas/US_States_Game/main.py
+++ Pandas/US_States_Game/main.py
@@ -11,24 +11,18 @@ image = "./blank_states_img.gif"
 my_screen.addshape(image)  
 img_turtle.shape(image)
 
-# Loading Data From CSV File
-
+# Loading States Data From CSV File
 data = pandas.read_csv("50_states.csv")
 US_states = pandas.unique(data["state"])
 guessed_states = []
-states_to_learn = []
-score = 0
 
 while len(guessed_states) < 50 :
     user_input = my_screen.textinput(title= f"{len(guessed_states)}/50 States Correct !!", prompt="What's the another state? ")
     answer_state = user_input.title()
 
     if answer_state == "Exit" :
-        for state in US_states :
-            if state not in guessed_states:
-                states_to_learn.append(state)
-        # Saving the states which we have to learn in CSV File.
-        pandas.DataFrame(states_to_learn).to_csv("states_to_learn.csv") 
+        states_to_learn = [state for state in US_states if state not in guessed_states]    
+        pandas.DataFrame(states_to_learn).to_csv("states_to_learn.csv") # Saving the states which we have to learn in CSV File.
         break
 
     if answer_state in US_states :
@@ -43,8 +37,6 @@ while len(guessed_states) < 50 :
         writing_turtle.write(answer_state)
 
         guessed_states.append(answer_state)
-        score+=1
 
-my_screen.mainloop()  # TO keep the screen open
-
-# my_screen.exitonclick()     # To close the screen when we click on it.
+# my_screen.mainloop()  # TO keep the screen open
+# my_screen.exitonclick()  # To close the screen when we click on it.
