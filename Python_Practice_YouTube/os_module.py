b7282d177b22b9ac0d923496b088617b2296b8bb
Sheenu Bansal <sheenubansal308@gmail.com>
2021-12-22 00:39:37 +0530

    Basic functionalities with help of os module of python

---
+++ Python_Practice_YouTube/os_module.py
@@ -0,0 +1,24 @@
+import os
+
+# print(dir(os))
+# print(os.getcwd())
+# os.chdir("C://")
+# print(os.getcwd())
+# print(os.listdir())
+# print(os.listdir("C://"))
+# os.mkdir("This_directory")
+# os.makedirs("This_directory/that_directory")  #it makes sub directories also
+# os.rename("file1.txt","file2.txt")
+# os.makedirs("This_directory/that_directory/jingalala.txt") # if u will try this, it will create "jingalala.txt" folder not file  
+# print(os.environ.get('Path'))
+# print(os.environ.get('OneDrive'))
+# print(os.environ.get('TEMP'))
+# print(os.environ.get('NUMBER_OF_PROCESSORS'))
+# print(os.path.exists("H:/100_days_of_Python/Python_Practice_YouTube/os_module.py"))
+# print(os.path.exists("H:\\100_days_of_Python\\Python_Practice_YouTube\\os_module.py"))
+# print(os.path.join("H:\\100_days_of_Python\\Python_Practice_YouTube\\","file1.txt"))
+# os.path.join("H:\\100_days_of_Python\\Python_Practice_YouTube\\","file1.txt")
+# print(os.getenv("Path"))
+# print(os.getlogin())
+print(os.getpid())
+print(os.getppid())
